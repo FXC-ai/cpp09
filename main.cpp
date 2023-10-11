1fdf46c81c75ecdd5dafc5bfec314c61cf390cd7
Francois-xavier Coindreau <fcoindre@c1r5s7.42lausanne.ch>
2023-10-11 16:27:57 +0200

    in p...

--- main.cpp
+++ main.cpp
@@ -7,68 +7,56 @@
 
 class Display
 {
-	public :
-		template <typename T>
-		void operator()(T & a)
-		{
-			std::cout << a << " - ";
-		}
+public:
+    template <typename T>
+    void operator()(T & a)
+    {
+        std::cout << a << " - ";
+    }
 };
 
 class fillMultimap
 {
+	public:
+		fillMultimap(std::multimap<std::string, std::string> & mp) : _mp(mp) {}
 
-	public :
-		fillMultimap(std::string data, std::multimap<std::string, std::string> & mp) : _mp(mp)
+		void operator()(const std::string & data)
 		{
 			int ind_coma = data.find(',');
-			this->_date = data.substr(0, ind_coma);
-			this->_exchange_rate = data.substr(ind_coma +1 , data.size());
-		};
-
-		void operator()()
-		{
-			this->_mp.insert(std::make_pair(this->_date, this->_exchange_rate));
+			std::string date = data.substr(0, ind_coma);
+			std::string exchange_rate = data.substr(ind_coma + 1, data.size());
+			this->_mp.insert(std::make_pair(date, exchange_rate));
 		}
 
-	private :
-		std::string _date;
-		std::string _exchange_rate;
+	private:
 		std::multimap<std::string, std::string> & _mp;
-
-
 };
 
 int main()
 {
-	std::ifstream csv_file("data.csv");
-	std::istream_iterator<std::string> it(csv_file);
-	std::istream_iterator<std::string> end;
+    std::ifstream csv_file("data.csv");
+    std::istream_iterator<std::string> it(csv_file);
+    std::istream_iterator<std::string> end;
 
-	std::multimap<std::string, std::string> data_map;
-	
-	for_each(it, end, fillMultimap(*it, data_map));
-	
-	// while (it!=end)
-	// {
+    std::multimap<std::string, std::string> data_map;
+    
+	++it;
 
-	// 	std::string data;
+    std::for_each(it, end, fillMultimap(data_map));
 
-	// 	data = *it;
+    std::multimap<std::string, std::string>::iterator itr;
 
-	// 	int ind_coma = (*it).find(',');
+	int h = 0;
+    for (itr = data_map.begin(); itr != data_map.end(); ++itr)
+	{
+        std::cout << itr->first << " => " << itr->second << std::endl;
+		h++;
+    }
 
-	// 	std::string date = data.substr(0, ind_coma);
-	// 	std::string exchange_rate = data.substr(ind_coma +1 , (*it).size());
-
-	// 	std::cout << date << " => " << exchange_rate << std::endl;
-
-	// 	/* Here is some parsing */
-
-	// 	++it;
-	// }
+	std::cout << h << std::endl;
 
 
 
-	return 0;
+
+    return 0;
 }

a1f41d28a7e0117d4958872a6a2f202ac9866d4d
Francois-xavier Coindreau <fcoindre@c1r5s7.42lausanne.ch>
2023-10-11 15:02:05 +0200

    ex 00 in slow progression

--- main.cpp
+++ main.cpp
@@ -15,33 +15,40 @@ class Display
 		}
 };
 
+template <typename T, typename U>
+class fillMultimap
+{
+
+	public :
+		fillMultimap(const T key, const U value, std::multimap<T, U> & mp) : _date(key), _exchange_rate(value), _mp(mp)
+		{};
+
+		void operator()(std::multimap<T, U> mp)
+		{
+			this->_mp.insert(std::make_pair(this->_date, this->_exchange_rate));
+		}
+
+		
+	private :
+		const T _date;
+		const U _exchange_rate;
+		std::multimap<T, U> & _mp;
+
+
+};
+
 int main()
 {
 	std::ifstream csv_file("data.csv");
-
-
 	std::istream_iterator<std::string> it(csv_file);
 	std::istream_iterator<std::string> end;
-
-	std::map<std::string, std::string> data_map;
-
 
 	while (it!=end)
 	{
 
-
-		// std::stringstream ss(*it);
-    	// std::string date;
-		// while (!ss.eof())
-		// {
-		// 	getline(ss, date, ',');
-		// 	std::cout << date << std::endl;
-		// }
-
 		std::string data;
 
 		data = *it;
-
 
 		int ind_coma = (*it).find(',');
 
@@ -49,13 +56,15 @@ int main()
 		std::string exchange_rate = data.substr(ind_coma +1 , (*it).size());
 
 		std::cout << date << " => " << exchange_rate << std::endl;
+
+		/* Here is some parsing */
+
 		++it;
 	}
 
+	std::multimap<std::string, std::string> data_map;
 
-
-	
-
+	//generate(it, end, fillMultimap());
 
 	return 0;
 }

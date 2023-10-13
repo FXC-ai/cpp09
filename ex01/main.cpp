f5895066e1fc2b5454a923d64831fc9ff366ca48
Francois-xavier Coindreau <fcoindre@c1r5s7.42lausanne.ch>
2023-10-13 18:38:04 +0200

    need to split main of ex01

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -4,118 +4,212 @@
 #include <sstream>
 #include <stack>
 #include <filesystem>
+#include <limits.h>
 
-
-// class RPN
-// {
-// 	public :
-// 		RPN(const std::string str);
-// 		void process();
-
-// 	private :
-// 		const std::string _str;
-// 		std::stack<long> stack_process;
-// };
-
-
-// RPN::RPN(const std::string str) : _str(str) {};
-
-
-
-void process (std::string str)
+class RPN
 {
+	public :
+		RPN(const std::string str);
+		void process();
+		void display();
 
-	std::stack<long> stack_process;
+		class DivisionByZero : public std::exception
+		{
+			virtual const char* what() const throw()
+			{
+				return "Error : division by zero.";
+			}
+		};
 
-	std::istringstream str_stream(str);
+		class MaxExceeded : public std::exception
+		{
+			virtual const char* what() const throw()
+			{
+				return "Error : INT_MAX exceeded.";
+			}
+		};
 
-	char c;
-	int loop_c = 0;
+		class NotADigit : public std::exception
+		{
+			virtual const char* what() const throw()
+			{
+				return "Error : not a digit.";
+			}
+		};
+
+		class StackSizeError : public std::exception
+		{
+			virtual const char* what() const throw()
+			{
+				return "Error : too many operators.";
+			}
+		};
+
+
+	private :
+		const std::string _str;
+		std::stack<long> stack_process;
+		void process_add();
+		void process_min();
+		void process_div();
+		void process_mul();
+		void process_push(const char c);
+};
+
+RPN::RPN(const std::string str) : _str(str) {};
+
+
+void RPN::process_push(const char c)
+{
+	if (std::isdigit(c) == false)
+	{
+		throw NotADigit();
+	}
+	this->stack_process.push(c - '0');
+}
+
+
+
+void RPN::process_add()
+{
 	long op0;
 	long op1;
-
-	while (str_stream >> c)
-	{
-
-		switch (c)
-		{
-			case 43 :
-				if (stack_process.size() <= 1)
-				{
-					throw std::exception();
-				}
-				op0 = stack_process.top();
-				stack_process.pop();
-				op1 = stack_process.top();
-				stack_process.pop();
-				stack_process.push((op1 + op0));
-				break;
-			case 45 :
-				if (stack_process.size() <= 1)
-				{
-					throw std::exception();
-				}
-				op0 = stack_process.top();
-				stack_process.pop();
-				op1 = stack_process.top();
-				stack_process.pop();
-				stack_process.push((op1 - op0));
-				break;
-			case 42 :
-				if (stack_process.size() <= 1)
-				{
-					throw std::exception();
-				}
-				op0 = stack_process.top();
-				stack_process.pop();
-				op1 = stack_process.top();
-				stack_process.pop();
-				stack_process.push((op1 * op0));
-				break;
-			case 47 :
-				if (stack_process.size() <= 1)
-				{
-					throw std::exception();
-				}
-				op0 = stack_process.top();
-				stack_process.pop();
-				op1 = stack_process.top();
-				stack_process.pop();
-				if (op0 == 0)
-				{
-					throw std::exception();
-				}
-				stack_process.push((op1 / op0));
-				break;
-			default :
-				stack_process.push(c - '0');
-				break;
-
-		}
-		
-		loop_c ++;
-	}
+	long r;
 	
-	if (stack_process.size() == 1)
+	if (this->stack_process.size() <= 1)
 	{
-		std::cout << "result = " << stack_process.top() << std::endl;
+		throw StackSizeError();
+	}
+	op0 = this->stack_process.top();
+	this->stack_process.pop();
+	op1 = this->stack_process.top();
+	this->stack_process.pop();
+	r = op1 + op0;
+	if (r < INT_MIN || r > INT_MAX)
+	{
+		throw MaxExceeded();
+	}
+	this->stack_process.push(r);
+}
+
+void RPN::process_min()
+{
+	long op0;
+	long op1;
+	long r;
+	
+	if (this->stack_process.size() <= 1)
+	{
+		throw StackSizeError();
+	}
+	op0 = this->stack_process.top();
+	this->stack_process.pop();
+	op1 = this->stack_process.top();
+	this->stack_process.pop();
+	r = op1 - op0;
+	if (r < INT_MIN || r > INT_MAX)
+	{
+		throw MaxExceeded();
+	}
+	this->stack_process.push(r);
+}
+
+void RPN::process_mul()
+{
+	long op0;
+	long op1;
+	long r;
+	
+	if (this->stack_process.size() <= 1)
+	{
+		throw StackSizeError();
+	}
+	op0 = this->stack_process.top();
+	this->stack_process.pop();
+	op1 = this->stack_process.top();
+	this->stack_process.pop();
+	r = op1 * op0;
+	if (r < INT_MIN || r > INT_MAX)
+	{
+		throw MaxExceeded();
+	}
+	this->stack_process.push(r);
+}
+
+void RPN::process_div()
+{
+	long op0;
+	long op1;
+	long r;
+	
+	if (this->stack_process.size() <= 1)
+	{
+		throw StackSizeError();
+	}
+	op0 = this->stack_process.top();
+	this->stack_process.pop();
+	op1 = this->stack_process.top();
+	this->stack_process.pop();
+	if (op0 == 0)
+	{
+		throw DivisionByZero();
+	}
+	r = op1 / op0;
+	if (r < INT_MIN || r > INT_MAX)
+	{
+		throw MaxExceeded();
+	}
+	this->stack_process.push(r);
+}
+
+void RPN::display()
+{
+	if (this->stack_process.size() == 1)
+	{
+		std::cout << "Result = " << this->stack_process.top() << std::endl;
 	}
 	else
 	{
 		std::cout << "Stack is not empty : ";
-		while (stack_process.empty() == false)
+		while (this->stack_process.empty() == false)
 		{
-			std::cout << stack_process.top() << " ";
-			stack_process.pop();
+			std::cout << this->stack_process.top() << " ";
+			this->stack_process.pop();
 		}
 		std::cout << std::endl;
 	}
-};
+}
 
-// * : 42
-// / : 47
-// - : 45
-// + : 43
+void RPN::process ()
+{
+	std::istringstream str_stream(this->_str);
+
+	char c;
+	int loop_c = 0;
+
+	while (str_stream >> c)
+	{
+		switch (c)
+		{
+			case '+' :
+				this->process_add();
+				break;
+			case '-' :
+				this->process_min();
+				break;
+			case '*' :
+				this->process_mul();
+				break;
+			case '/' :
+				this->process_div();
+				break;
+			default :
+				this->process_push(c);
+				break;
+		}	
+		loop_c ++;
+	}
+};
 
 int main(int argc, char* argv[])
 {
@@ -128,23 +222,12 @@ int main(int argc, char* argv[])
 
 	std::cout << argv[1] << std::endl << std::endl;
 
-	std::stack<int> stack_process;
+	RPN rpn(argv[1]);
 
-	std::istringstream str_stream(argv[1]);
-
-	// char c;
-	// int loop_c = 0;
-	// while (str_stream >> c)
-	// {
-	// 	std::cout << loop_c << " : " << c << std::endl;
-	// 	stack_process.push(c);
-	// 	loop_c ++;
-	// }
-
-	// std::cout << std::endl;
 	try
 	{
-		process(argv[1]);
+		rpn.process();
+		rpn.display();
 		
 	}
 	catch(const std::exception& e)
@@ -152,14 +235,5 @@ int main(int argc, char* argv[])
 		std::cerr << e.what() << '\n';
 	}
 	
-	// std::cout << std::endl;
-
-	// while (stack_process.empty() == false)
-	// {
-	// 	std::cout << static_cast<char> (stack_process.top())<< " : " << stack_process.top() << std::endl;
-	// 	stack_process.pop();
-	// }
-
-
     return 0;
 }

b0b941406e2f4d3e4c95b15aa8a2427f327cd769
Francois-xavier Coindreau <fcoindre@c1r5s7.42lausanne.ch>
2023-10-17 17:31:22 +0200

    Stop ca y est j'arrete de penser je vais courir

--- ex02/Acess_list.hpp
+++ ex02/Acess_list.hpp
@@ -20,6 +20,22 @@ class Acess_list : public std::list<T>
 			}
 			return it;
 		}
+
+
+
 };
+
+	// Acess_list<int> AL_test;
+
+
+	// AL_test.push_back(18);
+	// AL_test.push_back(12);
+	// AL_test.push_back(16);
+	// AL_test.push_back(98);
+	// AL_test.push_back(19);
+
+	// Acess_list<int>::iterator it = AL_test[20];
+
+	// std::cout << *it << std::endl;
 
 #endif
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -12,6 +12,7 @@ class DisplayList
 		}
 };
 
+
 template <typename T>
 void display(T & a)
 {
@@ -27,8 +28,6 @@ void swap_in_list(std::list<unsigned int>::iterator & it, std::list<unsigned int
 	*it_2 = temp;
 };
 
-
-
 class PmergeMe
 {
 	public :
@@ -41,7 +40,32 @@ class PmergeMe
 
 	private :
 		std::list<unsigned int>_list_to_sort;
+		void switch_it(std::list<unsigned int>::iterator & it, std::list<unsigned int>::iterator & it_2);
+		std::list<unsigned int>::iterator get_element_in_list(unsigned long ind, std::list<unsigned int> & list);
+
 };
+
+void PmergeMe::switch_it(std::list<unsigned int>::iterator & it, std::list<unsigned int>::iterator & it_2)
+{
+	unsigned int temp;
+
+	temp = *it;
+	*it = *it_2;
+	*it_2 = temp;
+};
+
+std::list<unsigned int>::iterator PmergeMe::get_element_in_list(unsigned long ind, std::list<unsigned int> & list)
+{
+	std::list<unsigned int>::iterator it;
+	it = list.begin();
+
+	while (ind > 0)
+	{
+		it++;
+		ind--;
+	}
+	return it;
+}
 
 
 PmergeMe::PmergeMe(std::list<unsigned int>list_to_sort) : _list_to_sort(list_to_sort){};
@@ -61,23 +85,43 @@ void PmergeMe::sort_pairs()
 	std::list<unsigned int>::iterator it = this->_list_to_sort.begin();
 	std::list<unsigned int>::iterator it_2 = this->_list_to_sort.begin();
 
-	unsigned int temp;
+	unsigned long loop_count = this->_list_to_sort.size() / 2;
+
+	//unsigned int temp;
 	it_2++;
 	// FONCTIONNE PAS QUAND IL Y A UN NBR IMPAIR DE VALEURS !!!!!!
-	while (it_2 != this->_list_to_sort.end())
+
+	while (loop_count > 0)
 	{	
-		std::cout << "it = " << *it << " it_2 = " << *it_2 <<std::endl;
 		if (*it_2 < *it)
 		{	
-			temp = *it;
-			*it = *it_2;
-			*it_2 = temp;
+			this->switch_it(it, it_2);
 		}
 		++it;
 		++it;
 		++it_2;
 		++it_2;
-	}	
+		loop_count--;
+	}
+
+	//DEBUG
+	int d = 0;
+	std::cout << "After sorting pairs :" << std::endl;
+	for (std::list<unsigned int>::iterator it = this->_list_to_sort.begin(); it != this->_list_to_sort.end(); ++it)
+	{
+		std::cout << *it;
+		if (d % 2 == 1)
+		{
+			std::cout << " | ";
+		}
+		else
+		{
+			std::cout << "-";
+		}
+		d++;
+	}
+	std::cout << std::endl;
+	std::cout << std::endl;
 }
 
 void PmergeMe::insertion_sort_pairs ()
@@ -85,19 +129,14 @@ void PmergeMe::insertion_sort_pairs ()
 	// std::list<unsigned int>::interator start = this->_list_to_sort.begin();
 
 	std::list<unsigned int>::iterator it = this->_list_to_sort.begin();
-	++it;
 	
-	// std::list<unsigned int>::iterator it_2 = this->_list_to_sort.begin();
-	// ++it_2;
-	// ++it_2;
-	// ++it_2;
-	
+
 	// creer un liste S avec les plus grands nombres de chaque paire.
 	// analyser chaque nombre au sein de cette liste S
 	// inserer les paires en conséquence dans la liste initiale
 
 
-	std::list<int> S;
+	std::list<unsigned int> S;
 
 	for (unsigned long i = 0; i < this->_list_to_sort.size(); ++i)
 	{
@@ -108,30 +147,134 @@ void PmergeMe::insertion_sort_pairs ()
 		++it;
 	}
 
-	for_each(S.begin(), S.end(), DisplayList());
 
-	// while (it_2 != this->_list_to_sort.end())
-	// {
-	// 	if (*it > *it_2)
-	// 	{
-	// 		swap_in_list(it, it_2);
-	// 		it--;
-	// 		it_2--;
-	// 		swap_in_list(it, it_2);
-	// 		it++;
-	// 		it_2++;
-	// 	}
-	// 	it++;
-	// 	it++;
-	// 	it_2++;
-	// 	it_2++;
-	// }
+	for (unsigned long index = 1;index < S.size(); ++index)
+	{
+		std::cout << std::endl;
+		for_each(S.begin(), S.end(), DisplayList());
+		std::cout << std::endl;
+
+		std::list<unsigned int>::iterator start = S.begin();
+		std::list<unsigned int>::iterator it_current = this->get_element_in_list(index, S);
+		std::list<unsigned int>::iterator it_mover = this->get_element_in_list(index - 1, S);
+		unsigned long ind_to_insert = 0;
+
+		std::cout << "Ici on compare le current " << *it_current << " avec le mover " << *it_mover <<std::endl;
+		while (*it_current < *it_mover)
+		{
+			ind_to_insert++;
+
+			if (it_mover != start)
+			{
+				--it_mover;
+			}
+			else
+			{
+				break;
+			}
+		}
+	
+
+		std::cout << "il faut reculer " << *it_current << " de " << ind_to_insert << " places." << " cad avant "<< *it_mover <<std::endl;
+
+
+		// std::cout << index << " : " << " current value = " << *it_current << std::endl;
+
+		// unsigned int temp_value;
+		// temp_value = *it_current;
+		// --it_current;
+
+		// unsigned long ind_to_insert = 0;
+
+		// std::cout << "Ici on compare " << temp_value << " avec " << *it_current <<std::endl;
+		// while (temp_value < *it_current && it_current != start)
+		// {
+		// 	ind_to_insert++;
+		// 	--it_current;
+		// }
+
+		// std::list<unsigned int>::iterator it_to_insert = this->get_element_in_list(index - ind_to_insert, S);
+		
+		// std::cout << "il faut reculer " << temp_value << " de " << ind_to_insert << " places." << " cad avant "<< *it_to_insert <<std::endl;
+
+		// if (ind_to_insert != 0)
+		// {
+		// 	//break;
+		// 	//it_to_insert--;
+		// 	if (it_to_insert == start)
+		// 	{
+		// 		S.erase(this->get_element_in_list(index, S));
+		// 		S.push_front(temp_value);
+		// 	}
+		// 	else
+		// 	{
+		// 		S.erase(this->get_element_in_list(index, S));
+		// 		S.insert(it_to_insert, temp_value);
+		// 	}
+
+		// 	//S.insert(it_to_insert, temp_value);
+		// 	std::cout << std::endl;
+		// 	for_each(S.begin(), S.end(), DisplayList());
+		// 	std::cout << std::endl;
+
+		// }
+		// while (start != it_current)
+		// {
+		// 	--it_current;
+
+		// 	std::cout << "Analyse de " << temp_value << " et " << *it_current << " : " << std::endl;
+		// 	if (temp_value > *it_current)
+		// 	{
+		// 		std::cout << "index = " << index << std::endl;
+		// 		std::cout << "ind_to_insert = " << ind_to_insert << std::endl;
+
+		// 		std::list<unsigned int>::iterator it_to_insert = this->get_element_in_list((index - ind_to_insert -1), S);
+
+		// 		std::cout << "Je veux switch " << *it_to_insert << " " << *it_current <<std::endl;
+		// 		std::cout << std::endl;
+
+
+		// 		break;
+		// 	}
+		// 	else
+		// 	{
+		// 		std::cout << "La faut insert..." <<std::endl;
+		// 		ind_to_insert++;
+		// 	}
+		// }
+	}
+
+
+	std::cout << std::endl;
+	for_each(S.begin(), S.end(), DisplayList());
+	std::cout << std::endl;
+
+
+	//DEBUG
+	int d = 0;
+	std::cout << std::endl;
+	std::cout << "After insertion_sort_pairs :" << std::endl;
+	for (std::list<unsigned int>::iterator it = this->_list_to_sort.begin(); it != this->_list_to_sort.end(); ++it)
+	{
+		std::cout << *it;
+		if (d % 2 == 1)
+		{
+			std::cout << " | ";
+		}
+		else
+		{
+			std::cout << "-";
+		}
+		d++;
+	}
+	std::cout << std::endl;
 
 }
 
 
 int main(int argc, char* argv[])
 {
+
 	if (argc <= 1)
 	{
 		std::cerr << "Error" <<std::endl;
@@ -159,11 +302,15 @@ int main(int argc, char* argv[])
 
 	PmergeMe pmm = PmergeMe(list_to_sort);
 
+	std::cout << "Input : ";
 	pmm.DisplayListToSort();
+	std::cout << std::endl;
 	std::cout << std::endl;
 
 	pmm.sort_pairs();
+	pmm.insertion_sort_pairs();
 
+	std::cout << std::endl;
 	pmm.DisplayListToSort();
 
 
